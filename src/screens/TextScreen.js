460569111993d440425b8b3aaeeebd316656d4c2
Wagner Gauer <wggauer@gmail.com>
2024-09-16 23:04:48 -0300

    Add another text iput for password and check if it is long enough

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
     const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
     console.log(name)
 
     return (
@@ -16,6 +17,16 @@ const TextScreen = () => {
                 value= {name}
             />
             <Text>My name is {name}</Text>
+            <Text>Enter Your Password</Text>
+            <TextInput 
+                style={styles.input}
+                autoCapitalize="none" // Both IOS and Android autocapitalize the first letter of a text input, this is not always desirable
+                autoCorrect={false} // The IOS autocorrects words so for certain text inputs like emails or passwords we do not want that
+                onChangeText={(newValue) => setPassword(newValue)} 
+                value= {password}
+            />
+            {password.length < 5 ? <Text> Your password must be at least 5 characters long</Text>: null}
+            
         </View>
     )
 }
